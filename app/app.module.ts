06ba9a8ab2e726e7b71260ec27c486341e2d1469
LeyMa <leyla.markosyan@gmail.com>
2016-11-03 15:51:13 +0400

    added new float-chart module

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,9 +5,11 @@ import {NgModule} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {BrowserModule} from "@angular/platform-browser";
 import {routing} from "./app.routing";
+import {DashboardComponent} from "./modules/dashboard/dashboard.component";
+import {FloatChartsComponent} from "./modules/float-charts/float-charts.component";
 @NgModule({
     imports: [BrowserModule,routing],
-    declarations: [AppComponent],
+    declarations: [AppComponent, DashboardComponent],
 
     bootstrap: [AppComponent]
 })
