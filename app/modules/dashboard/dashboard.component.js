23526f58ee75d046ace4b50354a2f13be242d793
LeyMa <leyla.markosyan@gmail.com>
2016-11-03 14:28:01 +0400

    added dashboard module

--- app/modules/dashboard/dashboard.component.js
+++ app/modules/dashboard/dashboard.component.js
@@ -8,19 +8,14 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-/**
- * Created by leyla.markosyan on 11/3/2016.
- */
-/**
- * Created by leyla.markosyan on 11/3/2016.
- */
 var directives_1 = require("@angular/core/src/metadata/directives");
 var PageWrapperComponent = (function () {
     function PageWrapperComponent() {
     }
     PageWrapperComponent = __decorate([
         directives_1.Component({
-            templateUrl: 'modules/page-wrapper.component.html'
+            moduleId: module.id,
+            templateUrl: 'page-wrapper.component.html'
         }), 
         __metadata('design:paramtypes', [])
     ], PageWrapperComponent);
--- app/modules/dashboard/dashboard.component.ts
+++ app/modules/dashboard/dashboard.component.ts
@@ -1,11 +1,6 @@
-/**
- * Created by leyla.markosyan on 11/3/2016.
- */
-/**
- * Created by leyla.markosyan on 11/3/2016.
- */
 import {Component} from "@angular/core/src/metadata/directives";
 @Component({
-    templateUrl: 'modules/page-wrapper.component.html'
+    moduleId:module.id,
+    templateUrl: 'page-wrapper.component.html'
 })
 export class PageWrapperComponent {}
